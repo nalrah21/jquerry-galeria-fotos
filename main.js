870fe65b83a57b4ed8ed4c05b35a0095f16ca89f
harlan <harlan.sa@hotmail.com>
2024-07-16 17:20:46 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    console.log(document.querySelector('header button'))
+    console.log($('#botao-cancelar'))
+
+    $('header button').click(function(){
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function(){
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const endercoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${endercoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${endercoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>   
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereco-imagem-nova').val('');
+    })
+})
